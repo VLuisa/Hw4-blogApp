1ba8e598585ae66383799a0a8537ad7c8c556c45
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-08-14 21:33:51 -0400

    fixed root_url

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,7 @@ export const ActionTypes = {
 
 // const ROOT_URL = 'https://cs52-simple-blog.herokuapp.com/api';
 // const ROOT_URL = 'http://localhost:9090/api';
-const ROOT_URL = 'https://cs52-my-blog.herokuapp.com/';
+const ROOT_URL = 'https://cs52-my-blog.herokuapp.com/api';
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
 const API_KEY = '?key=l_vasquez';
 
